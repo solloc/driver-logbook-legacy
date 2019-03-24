7669e758b77df6b39a5fe0b44153e07fb8d5e0df
solloc <matthias@maisch.me>
2019-03-24 18:13:15 +0100

    basic routes set

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,18 @@ from app import app
 @app.route('/index')
 def index():
     return "Hello, World! (Driver Log)"
+
+
+@app.route('/login')
+def login():
+    return "login"
+
+
+@app.route('/logout')
+def logout():
+    return "logout"
+
+
+@app.route('/register')
+def register():
+    return "register"
