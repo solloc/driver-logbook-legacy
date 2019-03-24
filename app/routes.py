0b979df8e177b423b1603cf155724b8e244b672b
solloc <matthias@maisch.me>
2019-03-24 18:41:53 +0100

    index template defined

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
     # return "Hello, World! (Driver Log)"
 
 
