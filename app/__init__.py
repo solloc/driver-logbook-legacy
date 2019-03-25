3a55a1fdb4db2630896c4d3ec1293822a080daf7
solloc <matthias@maisch.me>
2019-03-25 19:11:26 +0100

    user model created

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes  # noqa: E402,F401
+from app import routes, model  # noqa: E402,F401
