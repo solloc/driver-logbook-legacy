5647592f446de78429b438a9b1a42dab32c44e0b
solloc <matthias@maisch.me>
2019-04-01 20:06:59 +0200

    basic login implemented (#5)

--- app/forms.py
+++ app/forms.py
@@ -7,6 +7,13 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', [DataRequired()])
     email = StringField('E-Mail Address', [DataRequired()])
     password = PasswordField('Password', [DataRequired(),
-                                          EqualTo('password_confirm')])
+                                          EqualTo('password_confirm',
+                                                  'Passwords must match')])
     password_confirm = PasswordField('Password (repeat)')
     submit = SubmitField('Submit')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', [DataRequired()])
+    password = PasswordField('Password', [DataRequired()])
+    submit = SubmitField('Sign in')
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,30 @@
 from app import app, db
-from flask import render_template, redirect
-from app.forms import RegistrationForm
+from flask import render_template, redirect, url_for, flash
+from app.forms import RegistrationForm, LoginForm
 from app.model import User
+from flask_login import login_required, login_user, current_user
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html', title='Home')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return "login"
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
 
 @app.route('/logout')
