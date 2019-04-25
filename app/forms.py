c09d9d575fa776c9ae2424e4918d711f6c4b10a4
solloc <matthias@maisch.me>
2019-04-25 20:01:39 +0200

    standard html5 date picker used (#17)

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, DecimalField, \
-                    DateField
+from wtforms import StringField, PasswordField, SubmitField, DecimalField
 from wtforms.validators import DataRequired, EqualTo
+from wtforms.fields.html5 import DateField
 
 
 class RegistrationForm(FlaskForm):
